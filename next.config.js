6dd06b298e28e3bc01739aec1f2e5d5d0f672f55
Raul Martins <raul.martins@xpi.com.br>
2023-01-07 22:52:13 -0300

    feat: add domain image minimu cache TTL 10 min

--- next.config.js
+++ next.config.js
@@ -1,8 +1,15 @@
+const DOMAIN_IMAGES = process.env.NEXT_PUBLIC_MERCADO_LIVRE_DOMAIN_IMAGES;
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   experimental: {
     appDir: true,
+  },
+  images: {
+    domains: [DOMAIN_IMAGES],
+    formats: ['image/avif', 'image/webp'],
+    minimumCacheTTL: 610,
   },
 };
 
