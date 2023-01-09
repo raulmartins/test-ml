153aec8d0a80331f8b6217ca5a664471985104b0
Raul Martins <raul.martins@xpi.com.br>
2023-01-09 04:15:31 -0300

    refactor: update variables

--- next-seo.config.ts
+++ next-seo.config.ts
@@ -1,21 +1,23 @@
 import { DefaultSeoProps } from 'next-seo';
-const title = 'Seach items -  test from mercado livre';
-const description = 'Seach items -  test from mercado livre';
+const title = 'Descontaço janeiro 2023 no Mercado Livre Brasil';
+const description =
+  'Encontre o que esta buscando em Descontaco janeiro 2023. Todo o que você necessita está em Mercado Libre.';
+const site = process.env.VERCEL_URL;
 const config: DefaultSeoProps = {
   title,
   description,
   canonical: process.env.NEXT_PUBLIC_APLICATION_URL,
   openGraph: {
-    type: 'website',
+    type: site,
     locale: 'pt_BR',
-    url: process.env.NEXT_PUBLIC_BASE_URL,
+    url: process.env.NEXT_PUBLIC_APLICATION_URL,
     siteName: 'teste-mercado-livre',
     title,
     description,
   },
   twitter: {
     handle: '@handle',
-    site: '@site',
+    site,
     cardType: 'summary_large_image',
   },
 };
